51724593362475380e36a1d0522fe621a30c6ae4
Chris Montgomery <chr.montgomery@gmail.com>
2016-06-21 13:12:41 -0500

    updates to make it work with latest deps

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var bluebird = require('bluebird');
-var psi = bluebird.promisify(require('psi'));
+var psi = require('psi');
 var chalk = require('chalk');
-var URL = process.env.PSI_URL; //'https://store.wsj.com'
+var URL = process.env.PSI_URL; //'https://my.website.com'
 var MOBILE_THRESHOLD = process.env.PSI_MOBILE_THRESHOLD; //50
 var DESKTOP_THRESHOLD = process.env.PSI_DESKTOP_THRESHOLD; //79
 
@@ -13,32 +13,33 @@ bluebird.join(
   psi(URL, {
     nokey: 'true',
     strategy: 'desktop'
-  })
-)
-  .spread(function (mobile, desktop) {
-    var isError = false;
+  }),
+  function (mobile, desktop) {
+    var isError = false,
+      mobileSpeed = mobile.ruleGroups.SPEED.score,
+      desktopSpeed = desktop.ruleGroups.SPEED.score;
 
     console.log('=== MOBILE ===');
-    console.log(chalk.cyan('score:', chalk.yellow(mobile.score)));
+    console.log(chalk.cyan('score:', chalk.yellow(mobileSpeed)));
     console.log(chalk.cyan('pageStats:'), mobile.pageStats);
     console.log('=== DESKTOP ===');
-    console.log(chalk.cyan('score:', chalk.yellow(desktop.score)));
+    console.log(chalk.cyan('score:', chalk.yellow(desktopSpeed)));
     console.log(chalk.cyan('pageStats:'), desktop.pageStats);
 
-    if (mobile.score < MOBILE_THRESHOLD) {
-      console.log(chalk.red('ERROR: mobile score below threshold of', chalk.yellow(MOBILE_THRESHOLD)));
+    if (mobileSpeed < MOBILE_THRESHOLD) {
+      console.log(chalk.red('ERROR: mobile score of'), chalk.yellow(mobileSpeed), chalk.red('is below threshold of'), chalk.yellow(MOBILE_THRESHOLD));
       isError = true;
     }
-    if (desktop.score < DESKTOP_THRESHOLD) {
-      console.log(chalk.red('ERROR: desktop score below threshold of', chalk.yellow(DESKTOP_THRESHOLD)));
+    if (desktopSpeed < DESKTOP_THRESHOLD) {
+      console.log(chalk.red('ERROR: desktop score of'), chalk.yellow(desktopSpeed), chalk.red('is below threshold of'), chalk.yellow(DESKTOP_THRESHOLD));
       isError = true;
     }
     if (isError) {
       process.exit(1);
     }
     console.log(chalk.green('PSI scores passed'));
-  })
-  .catch(function (e) {
-    console.log(e);
-    process.exit(1);
-  });
+  }
+).catch(function (e) {
+  console.log(e);
+  process.exit(1);
+});
